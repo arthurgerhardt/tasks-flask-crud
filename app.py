6f571a94a528fa8e1b725944402cc403a86f55bc
Arthur Gerhardt <renewedjesus@gmail.com>
2025-05-08 09:42:53 -0300

    Aula de Python sobre Flask

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Flask funcionando com sucesso. Vamos para a programação."
+# CRUD
+# Create, Read, Update, Delete
+# Tabela: Tarefa
+
+tasks = []
 
 if __name__ == '__main__':
     app.run(debug=True)
