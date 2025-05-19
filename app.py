dfc8f27d87e81d4235690f194db754ae3e66c6b0
Arthur Gerhardt <renewedjesus@gmail.com>
2025-05-19 12:22:50 -0300

    Resolvendo problemas no código!

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, request
 from models.task import Task
-
 app = Flask(__name__)
 
 # CRUD
@@ -8,6 +7,12 @@ app = Flask(__name__)
 # Tabela: Tarefa
 
 tasks = []
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return 'Teste'
 
 if __name__ == '__main__':
     app.run(debug=True)
