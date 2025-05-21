ac3ff4ec193c9eefe97c92d7c6ca1f24be9d84f4
Arthur Gerhardt <renewedjesus@gmail.com>
2025-05-21 16:33:38 -0300

    Mais aulas de Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 app = Flask(__name__)
 
@@ -7,12 +7,34 @@ app = Flask(__name__)
 # Tabela: Tarefa
 
 tasks = []
+task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Teste'
+    new_data = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_data)
+    print(tasks )
+    return jsonify({"message": "Nova tarefa criada com sucesso."}), 201
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": tasks_list,
+        "total_tasks": len(tasks_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+         return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possível encontrar a atividade."}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
